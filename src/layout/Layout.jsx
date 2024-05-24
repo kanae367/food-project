2ebe8d5f20501b0e8685a138a75c8fe30fb6eedb
”kodokunawa” <kodokunawa@gmail.com>
2024-05-24 08:02:17 +0300

    полностью убрана резина, сайт выглядит одинаково для всех устройств

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,9 +5,7 @@ const Layout = ({children}) => {
     return(
         <>
             <Header/>
-            <main>
-                {children}
-            </main>
+            {children}
             <Footer/>
         </>
     )
